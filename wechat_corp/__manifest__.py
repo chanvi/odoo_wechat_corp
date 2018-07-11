febffde3d43c5dd184094dd23b69e00fcb9d4cad
chanvi <huajie8@foxmail.com>
2018-07-11 14:15:32 +0800

    修改模块配置文件

--- wechat_corp/__manifest__.py
+++ wechat_corp/__manifest__.py
@@ -2,12 +2,13 @@
 {
     'name': "wechat_corp",
     'author': "Chanvi",
-    # any module necessary for this one to work correctly
+    'website': 'https://github.com/chanvi',
+    'summary': "Odoo 企业微信应用对接模块，实现Oauth2网页授权登录，自定义odoo业务消息推送。",
     'depends': ['auth_oauth'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/inherit_web_login.xml',
         'views/inherit_res_users.xml',
